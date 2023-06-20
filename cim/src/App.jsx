46a635e0b03974e2a3183c4a733321870f167e83
Israel Perez <ip345622@gmail.com>
2023-06-20 11:04:59 -0500

    Cambio de lenguage

---
+++ cim/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    {/* TODO: REALIZAR EL CÓDIGO */}
+    </>
+  )
+}
+
+export default App
