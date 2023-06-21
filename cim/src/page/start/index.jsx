38efaf3aa4c3c22edb2769306f6f6649ee3dfc68
Alexander <alexdroide466@gmail.com>
2023-06-21 07:31:13 -0500

    Primeros pasos en inicio

--- cim/src/page/start/index.jsx
+++ cim/src/page/start/index.jsx
@@ -1,9 +1,11 @@
 import React from "react";
-
+import chcocoCalebaut from '../../assets/img/inicio/chocolateBallebaut.png';
 const Home = () => {
     return(
         <div>
-            <h1>Home</h1>
+            <div>
+                <img src={chcocoCalebaut} alt="" />
+            </div>
         </div>
     )
 }
