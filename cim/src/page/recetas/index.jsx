8b2be9692235af344e77043a28674fc69bb7a859
Alexander <alexdroide466@gmail.com>
2023-06-30 12:16:25 -0500

    recetasSanchezMartinez

--- cim/src/page/recetas/index.jsx
+++ cim/src/page/recetas/index.jsx
@@ -13,7 +13,7 @@ const Recipes = () => {
     return(
         <div>
             <div className='mb-24 flex justify-center items-center'>
-                <div className='absolute flex flex-col justify-center items-center gap-5'>
+                <div className='absolute flex flex-col justify-center items-center gap-5 bg-black w-full h-4/6 bg-opacity-40 text-white'>
                     <h1 className='text-5xl'>Recetas</h1>
                     <p className='text-lg text-center'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic quia impedit, <br />
                     in et quaerat saepe nulla, doloribus adipisci fugiat aperiam tenetur atque! Minima, minus animi in nihil sit accusantium praesentium.</p>
@@ -24,48 +24,102 @@ const Recipes = () => {
             <div className='grid grid-cols-3 gap-10'>
                 <div className='flex flex-col items-center mb-14 gap-5'>
                     <img className='w-4/5' src={brownie} alt="" />
+                    <div className="border w-4/5 border-[#EA8239] border-opacity-40 "></div>
                     <h3 className='text-[#EA8239] font-semibold text-2xl'>Brownies</h3>
-                    <button className='border-2 border-[black] px-14 py-3 font-bold'>Ver mas</button>
+                    <p className='text-center mx-16'>
+                        Suspendisse potenti. Praesent tempus ipsum in orci venenatis, quis gravida erat mattis. 
+                        Duis euismod lacus id laoreet luctus. Vivamus sed lorem sagittis, ultricies dui eget, pharetra purus. 
+                        Fusce ac orci sed arcu posuere commodo.
+                    </p>
+                    <button className='border-2 border-[black] border-opacity-50 px-14 py-3 font-bold hover:bg-black hover:text-white'>Ver mas</button>
                 </div>
                 <div className='flex flex-col items-center mb-14 gap-5'>
                     <img className='w-4/5' src={galletasM} alt="" />
+                    <div className="border w-4/5 border-[#EA8239] border-opacity-40 "></div>
                     <h3 className='text-[#EA8239] font-semibold text-2xl'>Galletas de Mantequilla</h3>
-                    <button className='border-2 border-[black] px-14 py-3 font-bold'>Ver mas</button>
+                    <p className='text-center mx-16'>
+                        Suspendisse potenti. Praesent tempus ipsum in orci venenatis, quis gravida erat mattis. 
+                        Duis euismod lacus id laoreet luctus. Vivamus sed lorem sagittis, ultricies dui eget, pharetra purus. 
+                        Fusce ac orci sed arcu posuere commodo.
+                    </p>
+                    <button className='border-2 border-[black] border-opacity-50 px-14 py-3 font-bold hover:bg-black hover:text-white'>Ver mas</button>
                 </div>
                 <div className='flex flex-col items-center mb-14 gap-5'>
                     <img className='w-4/5' src={ganache} alt="" />
+                    <div className="border w-4/5 border-[#EA8239] border-opacity-40 "></div>
                     <h3 className='text-[#EA8239] font-semibold text-2xl'>Ganache Clasico de Chocolate</h3>
-                    <button className='border-2 border-[black] px-14 py-3 font-bold'>Ver mas</button>
+                    <p className='text-center mx-16'>
+                        Suspendisse potenti. Praesent tempus ipsum in orci venenatis, quis gravida erat mattis. 
+                        Duis euismod lacus id laoreet luctus. Vivamus sed lorem sagittis, ultricies dui eget, pharetra purus. 
+                        Fusce ac orci sed arcu posuere commodo.
+                    </p>
+                    <button className='border-2 border-[black] border-opacity-50 px-14 py-3 font-bold hover:bg-black hover:text-white'>Ver mas</button>
                 </div>
                 <div className='flex flex-col items-center mb-14 gap-5'>
                     <img className='w-4/5' src={mixEasy} alt="" />
+                    <div className="border w-4/5 border-[#EA8239] border-opacity-40 "></div>
                     <h3 className='text-[#EA8239] font-semibold text-2xl'>Mix Easy Sponge Cake</h3>
-                    <button className='border-2 border-[black] px-14 py-3 font-bold'>Ver mas</button>
+                    <p className='text-center mx-16'>
+                        Suspendisse potenti. Praesent tempus ipsum in orci venenatis, quis gravida erat mattis. 
+                        Duis euismod lacus id laoreet luctus. Vivamus sed lorem sagittis, ultricies dui eget, pharetra purus. 
+                        Fusce ac orci sed arcu posuere commodo.
+                    </p>
+                    <button className='border-2 border-[black] border-opacity-50 px-14 py-3 font-bold hover:bg-black hover:text-white'>Ver mas</button>
                 </div>
                 <div className='flex flex-col items-center mb-14 gap-5'>
                     <img className='w-4/5' src={redVelvet} alt="" />
+                    <div className="border w-4/5 border-[#EA8239] border-opacity-40 "></div>
                     <h3 className='text-[#EA8239] font-semibold text-2xl'>Mix Harina Red Velvet</h3>
-                    <button className='border-2 border-[black] px-14 py-3 font-bold'>Ver mas</button>
+                    <p className='text-center mx-16'>
+                        Suspendisse potenti. Praesent tempus ipsum in orci venenatis, quis gravida erat mattis. 
+                        Duis euismod lacus id laoreet luctus. Vivamus sed lorem sagittis, ultricies dui eget, pharetra purus. 
+                        Fusce ac orci sed arcu posuere commodo.
+                    </p>
+                    <button className='border-2 border-[black] border-opacity-50 px-14 py-3 font-bold hover:bg-black hover:text-white'>Ver mas</button>
                 </div>
                 <div className='flex flex-col items-center mb-14 gap-5'>
                     <img className='w-4/5' src={tegralSatin} alt="" />
+                    <div className="border w-4/5 border-[#EA8239] border-opacity-40 "></div>
                     <h3 className='text-[#EA8239] font-semibold text-2xl'>Tegral Satin Creme Cake</h3>
-                    <button className='border-2 border-[black] px-14 py-3 font-bold'>Ver mas</button>
+                    <p className='text-center mx-16'>
+                        Suspendisse potenti. Praesent tempus ipsum in orci venenatis, quis gravida erat mattis. 
+                        Duis euismod lacus id laoreet luctus. Vivamus sed lorem sagittis, ultricies dui eget, pharetra purus. 
+                        Fusce ac orci sed arcu posuere commodo.
+                    </p>
+                    <button className='border-2 border-[black] border-opacity-50 px-14 py-3 font-bold hover:bg-black hover:text-white'>Ver mas</button>
                 </div>
                 <div className='flex flex-col items-center mb-14 gap-5'>
                     <img className='w-4/5' src={betun} alt="" />
+                    <div className="border w-4/5 border-[#EA8239] border-opacity-40 "></div>
                     <h3 className='text-[#EA8239] font-semibold text-2xl'>Betun de Mantequilla</h3>
-                    <button className='border-2 border-[black] px-14 py-3 font-bold'>Ver mas</button>
+                    <p className='text-center mx-16'>
+                        Suspendisse potenti. Praesent tempus ipsum in orci venenatis, quis gravida erat mattis. 
+                        Duis euismod lacus id laoreet luctus. Vivamus sed lorem sagittis, ultricies dui eget, pharetra purus. 
+                        Fusce ac orci sed arcu posuere commodo.
+                    </p>
+                    <button className='border-2 border-[black] border-opacity-50 px-14 py-3 font-bold hover:bg-black hover:text-white'>Ver mas</button>
                 </div>
                 <div className='flex flex-col items-center mb-14 gap-5'>
                     <img className='w-4/5' src={cremaPastelera} alt="" />
+                    <div className="border w-4/5 border-[#EA8239] border-opacity-40 "></div>
                     <h3 className='text-[#EA8239] font-semibold text-2xl'>Crema Patelera Clasica</h3>
-                    <button className='border-2 border-[black] px-14 py-3 font-bold'>Ver mas</button>
+                    <p className='text-center mx-16'>
+                        Suspendisse potenti. Praesent tempus ipsum in orci venenatis, quis gravida erat mattis. 
+                        Duis euismod lacus id laoreet luctus. Vivamus sed lorem sagittis, ultricies dui eget, pharetra purus. 
+                        Fusce ac orci sed arcu posuere commodo.
+                    </p>
+                    <button className='border-2 border-[black] border-opacity-50 px-14 py-3 font-bold hover:bg-black hover:text-white'>Ver mas</button>
                 </div>
                 <div className='flex flex-col items-center mb-14 gap-5'>
                     <img className='w-4/5' src={cupcake} alt="" />
+                    <div className="border w-4/5 border-[#EA8239] border-opacity-40 "></div>
                     <h3 className='text-[#EA8239] font-semibold text-2xl'>Cupcake de Mantequilla</h3>
-                    <button className='border-2 border-[black] px-14 py-3 font-bold'>Ver mas</button>
+                    <p className='text-center mx-16'>
+                        Suspendisse potenti. Praesent tempus ipsum in orci venenatis, quis gravida erat mattis. 
+                        Duis euismod lacus id laoreet luctus. Vivamus sed lorem sagittis, ultricies dui eget, pharetra purus. 
+                        Fusce ac orci sed arcu posuere commodo.
+                    </p>
+                    <button className='border-2 border-[black] border-opacity-50 px-14 py-3 font-bold hover:bg-black hover:text-white'>Ver mas</button>
                 </div>
             </div>
         </div>
--- cim/src/page/ubicaciones/index.jsx
+++ cim/src/page/ubicaciones/index.jsx
@@ -3,7 +3,53 @@ import React from "react";
 const Ubicaciones = () => {
     return(
         <div>
-            <h1>Ubicaciones</h1>
+            <div className="bg-[#EA8239]">
+                <h1>Ubicaciones</h1>
+                <p>Visitanos en nuestras sucursales</p>
+            </div>
+            <div>
+                <h3>Cancún</h3>
+                <div>
+                    <p>CIM Nichupte</p>
+                    <p>Horarios</p>
+                    <p>Lunes a Viernes 9:00 am a 8:00 pm</p>
+                    
+                </div>
+                <div>
+                    <p>CIM 135</p>
+                    <p>Horarios</p>
+                    <p>Lunes a Viernes 9:00 am a 8:00 pm</p>
+                </div>
+                <div>
+                    <p>CIM Centro</p>
+                    <p>Horarios</p>
+                    <p>Lunes a Viernes 9:00 am a 8:00 pm</p>
+                </div>
+            </div>
+            <div>
+                <h3>Playa del Carmen</h3>
+                <div>
+                    <h3>CIM Playa</h3>
+                    <p>Horarios</p>
+                    <p>Lunes a Viernes 9:00 am a 8:00 pm</p>
+                </div>
+            </div>
+            <div>
+                <h3>Merida</h3>
+                <div>
+                    <h3>CIM Merida</h3>
+                    <p>Horarios</p>
+                    <p>Lunes a Viernes 9:00 am a 8:00 pm</p>
+                </div>
+            </div>
+            <div>
+                <h3>Tulum</h3>
+                <div>
+                    <h3>CIM Tulum</h3>
+                    <p>Horarios</p>
+                    <p>Lunes a Viernes 9:00 am a 8:00 pm</p>
+                </div>
+            </div>
         </div>
     )
 }
